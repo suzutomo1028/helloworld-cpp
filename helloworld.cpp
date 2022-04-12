0980f693de8995567e3187d34f3767ce27dc2a92
suzutomo1028 <suzutomo1028@gmail.com>
2022-04-12 15:35:41 +0900

    "hello, world"プログラムを追加する

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+﻿// C++による"hello, world"プログラム
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello, world" << endl;
+
+    return EXIT_SUCCESS;
+}
